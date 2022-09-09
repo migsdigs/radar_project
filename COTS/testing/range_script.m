2f41a91d91f6f28257ad75a7f83df4493a331d7d
marcoCella <mcella@live.it>
2022-09-09 19:06:09 +0200

    Working range and velocity scripts

--- COTS/testing/range_script.m
+++ COTS/testing/range_script.m
@@ -7,7 +7,7 @@ close all; clear; clc;
 % Separate the sync data and radar backscatter data and take care of data
 % inversion by the sound card
 data = -y(:,1); % Radar backscatter data (received reflected signal)
-sync = -y(:,2); % Sync data (square waveform)
+sync = y(:,2); % Sync data (square waveform)
 
 % Parameters
 c = 299792458;                % Speed of light [m/s]
@@ -26,38 +26,12 @@ sync_pulse(:,2) = 1:1:length(sync_pulse(:,1)); % Set indexes
 
 time_temp = find(sync_pulse==1);
 
-% Vectorized approach -> not working
-% up_data = data(sync_pulse(:,1)==1);         % Use sync_pulse as a mask on the original data to get the up-chirp values
-% 
-% up_data_first = up_data(1:N:end);
-% % up_data_first(:,2) = time_temp(1:N:end);
-% ind_list = time_temp(1:N:end);
-% pulse_data = [];
-% for i=2:length(up_data_first)
-%     if ind_list(i)+N-1<=N
-%         first_el = up_data_first(i);
-%         chirp = [first_el, data(ind_list(i):ind_list(i)+N-1)'];
-%         pulse_data = cat(1, pulse_data, chirp);
-%     end
-% end
-% 
-% X = mod(-mod(length(pulse_data), N), N);         % Used to find the previous divisible value with respect to length(up_data)
-% up_data_cut = pulse_data(1:end-(N-X));           % Remove the last elements so that we can reshape up_data
-% up_data_parsed = reshape(up_data_cut, [], N)';
-% 
-% time = zeros(1,length(up_data_parsed));
-% j = 1;
-% for i = 1:N+1:length(time_temp)
-%     time(j) = time_temp(i) / Fs;
-%     j = j+1;
-% end
-
-% Classic for loop
-up_data_parsed = zeros([], N); % Pre-allocate matrix
+% Build the matrix
+up_data_parsed = zeros([], N); % Pre-allocate 
 time = zeros(1,[]);            % Same
 k = 1;
-for i = 11:(size(sync_pulse)-N) 
-    if sync_pulse(i,1) == 1 && mean(sync_pulse(i-10:i-1,1)) == 0 % First value of a row = first up-chirp value
+for i = 2:(size(sync_pulse)-N) 
+    if sync_pulse(i,1) == 1 && sync_pulse(i-1) == 0 % First value of a row = first up-chirp value
         up_data_parsed(k,:) = data(i:i+N-1)';
         time(1,k) = sync_pulse(i,2) / Fs;
         k = k + 1;
@@ -87,7 +61,7 @@ xlim([0 100]);
 xlabel('Range (m)');
 ylabel('Time (s)');
 colorbar;
-caxis([-50 0]);
+caxis([-30 0]);
 title("RTI without clutter rejection, Tp="+Tp+"ms, f_{start}="+f_start_plot+"GHz, f_{stop}="+f_stop_plot+"GHz");
 
 a2=subplot(1,2,2);
@@ -96,7 +70,7 @@ xlim([0 100]);
 xlabel('Range (m)');
 ylabel('Time (s)');
 colorbar;
-caxis([-50 0]);
+caxis([-40 0]);
 title("RTI with clutter rejection, Tp="+Tp+"ms, f_{start}="+f_start_plot+"GHz, f_{stop}="+f_stop_plot+"GHz");
 
 
--- COTS/testing/velocity_script.m
+++ COTS/testing/velocity_script.m
@@ -19,29 +19,41 @@ data_cut = data((N-X+1):end);           % Remove the first elements so that we c
 data_parsed = reshape(data_cut, N, [])';
 
 % MS Clutter Rejection
-final_data = bsxfun(@minus, data_parsed, mean(data_parsed, 1)); % Subtract column mean to each column
+final_data = bsxfun(@minus, data_parsed, mean(data_parsed, 2)); % Subtract column mean to each column
 
 % FFT 
 f = abs(fft(final_data, 4*N, 2));
 f = 20*log10(f);
-
 f = f(:,1:size(f, 2) / 2);
 
 % Normalization 1
 f1 = f - max(max(f));
-delta_f1 = linspace(0, Fs/2, size(f1,2)); 
-vel1 = (delta_f1*c)/(2 * f_center);
-time1 = linspace(1, Tp * size(f1,1), size(f1,1));
+delta_f1 = linspace(0, Fs/2, size(f1, 2)); 
+vel1 = (delta_f1 * c)/(2 * f_center);
+time1 = linspace(1, Tp * size(f1, 1), size(f1, 1));
 
 % Normalization 2
-f2 = bsxfun(@minus, f, max(f, 2)); 
-delta_f2 = linspace(0, Fs/2, size(f2,2)); 
-vel2 = (delta_f2*c)/(2 * f_center);
+f_row_max = max(f, [], 2);
+f2 = f - f_row_max;
+delta_f2 = linspace(0, Fs/2, size(f2, 2)); 
+vel2 = (delta_f2 * c)/(2 * f_center);
 time2 = linspace(1, Tp * size(f2,1), size(f2,1));
 
 % Plotting
+f_c_plot = f_center/1e9;
+
+a1=subplot(1,2,1);
 imagesc(vel1, time1, f1);
-caxis([-35 0]);
+caxis([-45 0]);
 colorbar;
 set(gca,'XLim',[0 40]);
 xlabel('Velocity [m/sec]'); ylabel('Time [sec]');
+title("Normalization 1, Pulse time T_p="+Tp+"s, Center Frequency fc="+f_c_plot+"GHz");
+
+a2=subplot(1,2,2);
+imagesc(vel2, time2, f2);
+caxis([-15 0]);
+colorbar;
+set(gca,'XLim',[0 40]);
+xlabel('Velocity [m/sec]'); ylabel('Time [sec]');
+title("Normalization 2, Pulse time T_p="+Tp+"s, Center Frequency fc="+f_c_plot+"GHz");
