f27ea7742f3f9d9eea4c0aebf7cbc61e953d95d9
Marco Cella <mcella@live.it>
2022-09-29 12:26:30 +0200

    RMA, not sure if it's functioning

--- COTS/testing/SAR.m
+++ COTS/testing/SAR.m
@@ -16,6 +16,7 @@ N = Tp*Fs;      %[ ] Number of samples per upchirp
 c = 299792458;  %[m/s] Speed of light
 fc = 2.43e9;    %[Hz] Carrier frequency
 lambda = c/fc;  %[m] WaveLength
+delta_x = lambda/2;
 fStart = 2.408e9;            % Start Frequency [Hz]
 fStop = 2.495e9;             % Stop Frequency  [Hz]
 
@@ -65,9 +66,85 @@ for k = 1:numberOfPositions
 end
 
 Kr = linspace(4*pi/c*fStart,4*pi/c*fStop,N);
-Xa = linspace(-(lambda/2*numberOfPositions)/2,(lambda/2*numberOfPositions)/2,numberOfPositions);
+Xa = linspace(-(delta_x*numberOfPositions)/2,(delta_x*numberOfPositions)/2,numberOfPositions);
 figure(), clf()
 imagesc(Kr,Xa,angle(windowed))
 colorbar;
 
 %% RANGE MIGRATION ALGORITHM
+% Add zero-padding
+padding = 2048;
+temp_zeros = zeros(padding, size(windowed,2));
+for i = 1:size(windowed,2)
+    index = round((padding-size(windowed,1))/2);
+    temp_zeros(index+1:(index + size(windowed,1)),i) = windowed(:,i);
+end
+windowed_padded = temp_zeros;
+
+data_matrix = fftshift(fft(windowed_padded,[],1),1);
+kx = linspace((-pi/delta_x),(pi/delta_x),(size(data_matrix,1)));
+
+% Plot the magnitude in dB of cross-range FFT
+figure();
+imagesc(Kr, kx, 20*log10(abs(data_matrix)), [max(max(20*log10(abs(data_matrix))))-40,max(max(20*log10(abs(data_matrix))))]);
+xlabel('K_r (rad/m)'); ylabel('K_x (rad/m)');
+colorbar;
+
+% Plot the phase of cross-range FFT
+figure();
+imagesc(Kr, kx, angle(data_matrix));
+xlabel('K_r (rad/m)'); ylabel('K_x (rad/m)');
+colorbar;
+
+% Stolt interpolation
+ky = zeros(padding, N);
+interpolated_dm = zeros(padding, padding/2);
+for i = 1:padding
+    ky(i,:) = sqrt(Kr.^2 - kx(i)^2);
+end
+
+kstart = floor(min(min(ky)));
+kstop = ceil(max(max(ky)));
+k_ye = linspace(kstart, kstop, 1024);
+
+for i = 1:padding
+    interpolated_dm(i,:) = (interp1(ky(i,:), data_matrix(i,:), k_ye));
+end
+
+interpolated_dm(isnan(interpolated_dm)) = 1e-30;
+
+% Plot the phase after Stolt interpolation
+figure();
+imagesc(k_ye, kx, angle(interpolated_dm)); 
+xlabel('K_y (rad/m)'); ylabel('K_x (rad/m)');
+colorbar;
+
+ifft_intep_dm = ifft2(interpolated_dm,(size(interpolated_dm,1)*4),(size(interpolated_dm,2)*4));
+
+dfy = c*(kstop - kstart)/(4*pi);
+r_max = ((4*c*padding/2)/(2*dfy));
+rail_r_max = padding * delta_x;
+
+d_range_1 = 1;
+d_range_2 = 100;
+c_range_1 = -25; 
+c_range_2 = 25;  
+flipped = fliplr(rot90(ifft_intep_dm));
+d_index1 = round((size(flipped,1)/r_max) * d_range_1);
+d_index2 = round((size(flipped,1)/r_max) * d_range_2);
+c_index1 = round((size(flipped,2)/rail_r_max) * (c_range_1+(rail_r_max/2)));
+c_index2 = round((size(flipped,2)/rail_r_max) * (c_range_2+(rail_r_max/2)));
+
+truncated = flipped(d_index1:d_index2, c_index1:c_index2);
+down_range = linspace(-d_range_1, -d_range_2, size(truncated,1));
+cross_range = linspace(c_range_1, c_range_2, size(truncated, 2));
+
+truncated_final = truncated;
+for j = 1:size(truncated,2)
+    truncated_final(:,j) = (truncated(:,j)').*(abs(down_range)).^2;
+end
+truncated_db = 20 * log10(abs(truncated_final));
+
+figure();
+imagesc(cross_range, down_range, truncated_db);
+caxis([-90 0]); colorbar;
