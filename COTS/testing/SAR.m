5079d54675ea11f2310885801d5df19f954207c4
migsdigs <miguelito2384@gmail.com>
2022-10-26 13:33:22 +0200

    cleaned SAR file

--- COTS/testing/SAR.m
+++ COTS/testing/SAR.m
@@ -9,54 +9,25 @@ close all; clear; clc;
 data = -y(:,1); % Radar backscatter data (received reflected signal)
 sync = -y(:,2); % Sync data (square waveform)
 
-Trp = 0.25;         % [s] Duration of measuring at each position
-Nrp = Trp*Fs;       % [ ] Number of samples at each position
-Tp = 20e-3;         % [s] Upchirp length
-N = Tp*Fs;          % [ ] Number of samples per upchirp
-c = 299792458;      % [m/s] Speed of light
-fc = 2.43e9;        % [Hz] Carrier frequency
-lambda = c/fc;      % [m] WaveLength
+Trp = 0.25;         % Duration of measuring at each position [s]
+Nrp = Trp*Fs;       % Number of samples at each position
+Tp = 20e-3;         % Upchirp length [s]
+N = Tp*Fs;          % Number of samples per upchirp
+c = 299792458;      % Speed of light [m/s]
+fc = 2.43e9;        % Carrier frequency [Hz]
+lambda = c/fc;      % WaveLength [m]
 delta_x = lambda/2;
 fStart = 2.408e9;   % Start Frequency [Hz]
 fStop = 2.495e9;    % Stop Frequency  [Hz]
 
-
-% sync = (sync > 0.25)'; % Set sync signal to 0 or 1
-% 
-% % Find start of positions. This is where there is at least N+1 zeros
-% % followed by N ones
-% sequenceToFind = [zeros(1,2*N), ones(1, 0.5*N)];
-% found = strfind(sync, sequenceToFind);
-% startIndices = found + 2*N + Nrp;
-% numberOfPositions = length(startIndices);
-% 
-% dataMatrix = zeros(numberOfPositions,Nrp);
-% syncMatrix = zeros(numberOfPositions,Nrp);
-% for k = 1:numberOfPositions
-%     dataMatrix(k,:) = data(startIndices(k):startIndices(k)+Nrp-1);
-%     syncMatrix(k,:) = sync(startIndices(k):startIndices(k)+Nrp-1);
-% end
-% 
-% % APPLYING HILBERT TRANSFORM
-% % First, integrate over all up-chirps
-% dataIntegrated = zeros(numberOfPositions,N);
-% sequenceToFind = [0, 0, 1, 1];
-% for k = 1:numberOfPositions
-%     upchirpStart = strfind(syncMatrix(k,:),sequenceToFind);
-%     for j = 1:(length(upchirpStart)-1)
-%         dataIntegrated(k,:) = dataIntegrated(k,:) + dataMatrix(k,(upchirpStart(j)+2):(upchirpStart(j)+1+N));
-%     end
-%     dataIntegrated(k,:) = dataIntegrated(k,:)/length(upchirpStart);
-% end
-
 % separate square waveform and data
-data_bs = data;
-data_sc = sync >= 0.25;
+data_backscatter = data;
+data_sync = sync >= 0.25;
 data_N = round(Tp * Fs);
 data_Nrp = ceil(Trp * Fs);
 
-% find begginings of positions
-data_pos_dif = diff([false; ~data_sc; false]);
+% find begining of positions
+data_pos_dif = diff([false; ~data_sync; false]);
 data_pos_len = find(data_pos_dif<0) - find(data_pos_dif>0);
 data_pos_idx = find(data_pos_dif>0);
 
@@ -66,46 +37,43 @@ data_pos_len = data_pos_len(data_pos_valid);
 data_pos_idx = data_pos_idx + data_pos_len-1;
 clear data_pos_dif data_pos_valid data_pos_len;
 
-% add the guard band
-%data_pos_idx = data_pos_idx + data_Nrp + 0.5*data_N;
-%figure(); plot(data_sc); hold on; xline(data_pos_idx, 'r');
-
-% assemble 2D matrices
 data_pos_idx = cell2mat(arrayfun(@(x) x:x+data_Nrp-1, data_pos_idx(1:end-1), 'UniformOutput', false));
-data_bs = data_bs(data_pos_idx);
-data_sc = data_sc(data_pos_idx);
+data_backscatter = data_backscatter(data_pos_idx);
+data_sync = data_sync(data_pos_idx);
 clear data_pos_idx;
 
-% set number of possitions
-data_P = size(data_bs, 1);
+% number of positions
+data_P = size(data_backscatter, 1);
 
-% process every position separatelly
+% positions processed
 sar_data = NaN(data_P, data_N);
-for i = 1:size(data_bs,1)
+for i = 1:size(data_backscatter,1)
     % find upchrip begginings
-    upchrip_idx = diff([false, data_sc(i, :), false]);
+    upchrip_idx = diff([false, data_sync(i, :), false]);
     upchrip_len = find(upchrip_idx<0) - find(upchrip_idx>0);
     upchrip_idx = find(upchrip_idx>0);
     upchrip_idx = upchrip_idx(upchrip_len >= 0.9 * data_N);
 
-    % average upchirp
-    upchrip = cell2mat(arrayfun(@(x) data_bs(i, x:x+data_N-1), upchrip_idx, 'UniformOutput', false));
+    % integrate upchirps
+    upchrip = cell2mat(arrayfun(@(x) data_backscatter(i, x:x+data_N-1), upchrip_idx, 'UniformOutput', false));
     sar_data(i, :) = mean(reshape(upchrip', data_N, length(upchrip_idx))', 1); %#ok<UDIM> 
 end
-clear upchrip_idx upchrip data_bs;
+clear upchrip_idx upchrip data_backscatter;
 
 dataIntegrated = sar_data;
 
-%Now for Hilbert transform:
-%FFT each row
+% HILBERT TRANSFORM
+% FFT each row
 dataFFT = fft(dataIntegrated,N,2);
-%IFFT positive frequencies
+
+% IFFT positive frequencies
 dataFFT = dataFFT(:,1:end/2);
 dataIFFT = ifft(dataFFT,N,2);
-%set NaN values to 1e-30
+
+% set NaN=1e-30
 dataIFFT(isnan(dataIFFT)) = 1e-30;
 
-%% APPLYING HANN WINDOWING
+% APPLYING HANN WINDOWING
 numberOfPositions = size(dataIntegrated,1);
 windowed = zeros(numberOfPositions,N);
 hanningWindow = hann(N)';
@@ -120,7 +88,7 @@ figure(), clf()
 imagesc(kr,xa,angle(windowed))
 colorbar;
 
-%% RANGE MIGRATION ALGORITHM
+% RANGE MIGRATION ALGORITHM
 % Add zero-padding
 padding = 2048;
 temp_zeros = zeros(padding, size(windowed,2));
@@ -174,13 +142,15 @@ dfy = c*(k_yezpad - k_yel)/(4*pi);
 r_max = ((4*c*padding/2)/(2*dfy));
 rail_r_max = padding * delta_x;
 
+constant = 3;
+
 d_range_1 = 1;
 d_range_2 = 100;
 c_range_1 = -25; 
 c_range_2 = 25;  
 flipped = fliplr(rot90(ifft_interp_dm)); 
-d_index1 = round((size(flipped,1)/r_max) * d_range_1 * 3);
-d_index2 = round((size(flipped,1)/r_max) * d_range_2 * 3);
+d_index1 = round((size(flipped,1)/r_max) * d_range_1 * constant);
+d_index2 = round((size(flipped,1)/r_max) * d_range_2 * constant);
 c_index1 = round((size(flipped,2)/rail_r_max) * (c_range_1+(rail_r_max/2)));
 c_index2 = round((size(flipped,2)/rail_r_max) * (c_range_2+(rail_r_max/2)));
 
